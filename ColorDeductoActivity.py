b12cabf1fd06f50751efb3f90f20051cff79b646
Saumya-Mishra9129 <saumyamishra9129853131@gmail.com>
2020-08-27 00:03:10 +0530

    Fix : Joiner can also initiate a new game

--- ColorDeductoActivity.py
+++ ColorDeductoActivity.py
@@ -316,7 +316,7 @@ class ColorDeductoActivity(activity.Activity):
 
     def _joined_cb(self, activity):
         ''' ...or join an exisiting share. '''
-        self.after_share_join(False)
+        self.after_share_join(True)
 
     def after_share_join(self, sharer):
 
